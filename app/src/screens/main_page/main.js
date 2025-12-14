5b8a896d4875fbbd627601247092977c127f4ff8
Tanay Doble <tanaydoble2402@gmail.com>
2025-12-14 12:26:52 +0530

    Some new pages and Main.JS

--- app/src/screens/main_page/main.js
+++ app/src/screens/main_page/main.js
@@ -1,1 +1,11 @@
+// Profile button navigation
+let profileBtn = document.getElementById('profile_button');
+profileBtn.addEventListener('click', () => {
+    window.location.href = '../profile/profile.html';
+});
 
+// Crop Suggestions button navigation
+let cropSuggestionsBtn = document.getElementById('crop_advice');
+cropSuggestionsBtn.addEventListener('click', () => {
+    window.location.href = '../crop_advice/advice.html';
+});
