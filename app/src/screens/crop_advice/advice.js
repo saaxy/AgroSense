f3d9f2e72e7eb06e2e12fb7479d7b9c7134ff3c1
Sakshi <sakshikhendad0811@gmail.com>
2025-12-21 17:30:05 +0530

    updated advice.html and advice.js

--- app/src/screens/crop_advice/advice.js
+++ app/src/screens/crop_advice/advice.js
@@ -1,16 +1,40 @@
-function getAdvice(){
-    const soil = document.getElementById("soil").value;
-    const season = document.getElementById("season").value;
-    const water = document.getElementById("water").value;
-    const region = document.getElementById("region").value;
 
-    if(soil ==="" || season === "" || water ==="" || region ===""){
-          result.innerHTML = "Please select all field";
-          result.style.color = red;
-          return;
-    }
+// connect html file to python 
 
-    let crops =[];
+async function getCrop(){
+    const data = {
 
-    
+        // data that will sent to python as json
+
+        N : document.getElementById("N").value,
+        P : document.getElementById("P").value,
+        K : document.getElementById("K").value,
+        temperature: document.getElementById("temp").value,
+        humidity: document.getElementById("humidity").value,
+        ph : document.getElementById("ph").value,
+        rainfall : document.getElementById("rainfall").value
+
+    };
+    // URL still needs to be added heref
+    const response = await fetch("URL", {
+        method: "POST",  //used for sending data
+        headers:{
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify(data)   //converts js object ->JSON string before sending to flask
+    });
+
+    const result = await response.json();
+    document.getElementById("result").innerText = "✅ Recommended Crop: " + result.recommended_crop;   //to display crop on page
 }
+
+document.getElementById("Crop-Form").addEventListener("submit", function(e){
+    e.preventDefault(); //stops page reload
+    getCrop();
+});
+
+//back to main page 
+const HomeBtn =  document.getElementById("return");
+HomeBtn.addEventListener('click',(event)=>{
+    window.location.href = '../main_page/index.html';
+})
