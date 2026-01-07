2b7f4eff627758b861ce84cad93cfe5a9766e2f4
Tanay Doble <tanaydoble2402@gmail.com>
2026-01-07 18:26:44 +0530

    Final commit of Base file

---
+++ app/src/screens/weather/weather.js
@@ -0,0 +1,14 @@
+const API_KEY=process.env.WEATHER_API_KEY;
+console.log("API Key:", API_KEY);
+// const url = `https://api.weatherstack.com/current?access_key=${API_KEY}&query=New Delhi`;
+// const options = {
+// 	method: 'GET'
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
