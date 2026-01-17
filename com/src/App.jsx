ca68fa58b9bac70cf03481130147f6cd76877039
Tanay Doble <tanaydoble2402@gmail.com>
2026-01-17 10:13:34 +0530

    Minor changes

--- com/src/App.jsx
+++ com/src/App.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
-
-=======
 import {
   Route,
   createBrowserRouter,
@@ -12,19 +5,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
->>>>>>> 437dc54b9abcfb0482dac9f7a5db47dcc9e44652
 
 const Router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<LoginPage />} />)
 );
 
-<<<<<<< HEAD
-export default App;
-
-=======
 const App = () => {
   return <RouterProvider router={Router} />;
 };
 
 export default App;
->>>>>>> 437dc54b9abcfb0482dac9f7a5db47dcc9e44652
