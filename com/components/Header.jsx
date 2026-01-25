80bc16811827a4ed5b891cf7d3fee2675c454439
Tanay Doble <tanaydoble2402@gmail.com>
2026-01-25 17:35:50 +0530

    Added first component

---
+++ com/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment jutifyContent="center" alignItems="center">
+      <img
+        src="../assets/logo.png"
+        alt="AgroSense Logo"
+        width="100%"
+        height="10%"
+      />
+    </Fragment>
+  );
+};
+
+export default Header;
